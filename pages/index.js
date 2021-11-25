4b24ecd4bf5b868c1aaf4b38a83d8b41c025f173
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2021-11-25 19:01:40 +0600

    fetch products & show ProductDetails

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,17 @@ import {
 } from "@material-ui/core";
 import Layout from "../components/Layout";
 import data from "../utils/data";
+import db from "../utils/db";
+import Product from "../models/Product";
 
-export default function Home() {
+export default function Home(props) {
+	const {products} = props;
+
 	return (
 		<Layout>
 			<h1>All Products</h1>
 			<Grid container spacing={2}>
-				{data.products.map((product) => (
+				{products.map((product) => (
 					<Grid item md={3} key={product.name}>
 						<Card>
 							<NextLink href={`product/${product.slug}`} passHref>
@@ -45,4 +49,15 @@ export default function Home() {
 			</Grid>
 		</Layout>
 	);
+}
+
+export async function getServerSideProps() {
+	await db.connect();
+	const products = await Product.find({}).lean();
+	await db.disconnect();
+	return {
+		props: {
+			products: products.map(db.convertDocToObj),
+		},
+	};
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Button, Card, Grid, Link, List, ListItem, Typography } from "@material-ui/core";
+import {
+	Button,
+	Card,
+	Grid,
+	Link,
+	List,
+	ListItem,
+	Typography,
+} from "@material-ui/core";
 import NextLink from "next/link";
-import { useRouter } from "next/router";
-import Image from 'next/image';
+// import { useRouter } from "next/router";
+import Image from "next/image";
 import Layout from "../../components/Layout";
-import data from "../../utils/data";
+// import data from "../../utils/data";
+import db from "../../utils/db";
+import Product from "../../models/Product";
 
-const Product = () => {
-	const router = useRouter();
-	const { slug } = router.query;
-	const product = data.products.find((pd) => pd.slug === slug);
+export default function ProductDetails(props) {
+	const { product } = props;
+	// const router = useRouter();
+	// const { slug } = router.query;
+	// const product = data.products.find((pd) => pd.slug === slug);
 	if (!product) {
 		return (
 			<div>
@@ -19,55 +30,87 @@ const Product = () => {
 	}
 	return (
 		<Layout title={product.name} description={product.description}>
-      <NextLink href="/" passHref>
-        <Link>
-          <Typography>Back to Home</Typography>
-        </Link>
-      </NextLink>
+			<NextLink href="/" passHref>
+				<Link>
+					<Typography>Back to Home</Typography>
+				</Link>
+			</NextLink>
 			<Grid container spacing={2}>
-        <Grid item md={6} xs={12}>
-          <Image src={product.image} alt={product.name} height={300} width={300} layout="responsive"/>
-        </Grid>
-        <Grid item md={3} xs={12}>
-          <List>
-            <ListItem><Typography component="h1">{product.name}</Typography></ListItem>
-            <ListItem><Typography>Category: {product.category}</Typography></ListItem>
-            <ListItem><Typography>Rating: {product.rating} stars ({product.numReviews} reviews)</Typography></ListItem>
-            <ListItem><Typography>Description: {product.description}</Typography></ListItem>
-          </List>
-        </Grid>
-        <Grid item md={3} xs={12}>
-          <Card>
-            <List>
-              <ListItem>
-                <Grid container spacing={1}>
-                  <Grid item md={6}>
-                    <Typography>Price </Typography>
-                  </Grid>
-                  <Grid item md={6}>
-                    <Typography>{product.price}</Typography>
-                  </Grid>
-                </Grid>
-              </ListItem>
-              <ListItem>
-                <Grid container spacing={1}>
-                  <Grid item md={6}>
-                    <Typography>Status </Typography>
-                  </Grid>
-                  <Grid item md={6}>
-                    <Typography>{(product.countStock>1) ? "In Stock" : "Unavailable"}</Typography>
-                  </Grid>
-                </Grid>
-              </ListItem>
-              <ListItem>
-              <Button fullWidth variant="contained" color="primary">Add to Cart</Button>
-              </ListItem>
-            </List>
-          </Card>
-        </Grid>
-      </Grid>
+				<Grid item md={6} xs={12}>
+					<Image
+						src={product.image}
+						alt={product.name}
+						height={300}
+						width={300}
+						layout="responsive"
+					/>
+				</Grid>
+				<Grid item md={3} xs={12}>
+					<List>
+						<ListItem>
+							<Typography component="h1">{product.name}</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>Category: {product.category}</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>
+								Rating: {product.rating} stars ({product.numReviews} reviews)
+							</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>Description: {product.description}</Typography>
+						</ListItem>
+					</List>
+				</Grid>
+				<Grid item md={3} xs={12}>
+					<Card>
+						<List>
+							<ListItem>
+								<Grid container spacing={1}>
+									<Grid item md={6}>
+										<Typography>Price </Typography>
+									</Grid>
+									<Grid item md={6}>
+										<Typography>{product.price}</Typography>
+									</Grid>
+								</Grid>
+							</ListItem>
+							<ListItem>
+								<Grid container spacing={1}>
+									<Grid item md={6}>
+										<Typography>Status </Typography>
+									</Grid>
+									<Grid item md={6}>
+										<Typography>
+											{product.countStock > 1 ? "In Stock" : "Unavailable"}
+										</Typography>
+									</Grid>
+								</Grid>
+							</ListItem>
+							<ListItem>
+								<Button fullWidth variant="contained" color="primary">
+									Add to Cart
+								</Button>
+							</ListItem>
+						</List>
+					</Card>
+				</Grid>
+			</Grid>
 		</Layout>
 	);
-};
+}
 
-export default Product;
+export async function getServerSideProps(context) {
+	const { params } = context;
+	const { slug } = params;
+
+	await db.connect();
+	const product = await Product.findOne({ slug }).lean();
+	await db.disconnect();
+	return {
+		props: {
+			product: db.convertDocToObj(product),
+		},
+	};
+}
